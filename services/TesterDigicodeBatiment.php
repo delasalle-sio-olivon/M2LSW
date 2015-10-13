f41bc5e32beecdc874a60fff23bb65c9f6281d45
JolivetMaxime <contact@jolivetmaxime.fr>
2015-10-13 14:23:25 +0200

    Commentaire mise à jour

--- services/TesterDigicodeBatiment.php
+++ services/TesterDigicodeBatiment.php
@@ -2,14 +2,14 @@
 // Service web du projet Réservations M2L
 // Ecrit le 21/5/2015 par Jim
 
-// ce service web est appelé par le lecteur de digicode situé à l'entrée d'une salle,
+// ce service web est appelé par le lecteur de digicode situé à l'entrée du batiment de la M2L,
 // afin de tester la validité du digicode saisi par l'utilisateur
 
 // Le service web doit recevoir 2 paramètres : numSalle, digicode
 // Les paramètres peuvent être passés par la méthode GET (pratique pour les tests, mais à éviter en exploitation) :
-//     http://<hébergeur>/TesterDigicodeSalle.php?numSalle=10&digicode=123456
+//     http://<hébergeur>/TesterDigicodeBatiment.php?numSalle=10&digicode=123456
 // Les paramètres peuvent être passés par la méthode POST (à privilégier en exploitation pour la confidentialité des données) :
-//     http://<hébergeur>/TesterDigicodeSalle.php
+//     http://<hébergeur>/TesterDigicodeBatiment.php
 
 // déclaration des variables globales pour pouvoir les utiliser aussi dans les fonctions
 global $doc;		// le document XML à générer
@@ -86,7 +86,7 @@ function TraitementNormal()
 	$elt_data = $doc->createElement('data');
 	$doc->appendChild($elt_data);
 	// place l'élément 'reponse' juste après l'élément 'data'
-	$reponse = $dao->testerDigicodeBatiment($digicode);		// la fonction testerDigicodeSalle fournit "0" ou "1"
+	$reponse = $dao->testerDigicodeBatiment($digicode);		// la fonction testerDigicodeBatiment fournit "0" ou "1"
 	$elt_reponse = $doc->createElement('reponse', $reponse);
 	$elt_data->appendChild($elt_reponse);
 	return;
