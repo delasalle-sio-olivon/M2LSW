d9b294d88c01c10061fc1adb32cc2eb7f38b4310
delasalle-sio-olivon <delasalle.sio.olivon.a@gmail.com>
2015-11-03 13:56:28 +0100

    ConsulterRes Fin

--- vues/VueConsulterReservations.php
+++ vues/VueConsulterReservations.php
@@ -14,7 +14,7 @@
 		<div data-role="page">
 			<div data-role="header" data-theme="<?php echo $themeNormal; ?>">
 				<h4>M2L-GRR</h4>
-				<a href="index.php?action=Deconnecter">Déconnexion</a>
+				<a href="index.php?action=Menu">Retour</a>
 			</div>
 			<div data-role="content">
 				<h4 style="text-align: center; margin-top: 20px; margin-bottom: 20px;">Utilisateur : <?php echo $nom; ?></h4>
